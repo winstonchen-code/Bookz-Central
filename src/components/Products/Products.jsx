d4be5203d1beabb8a9d2b0a0acad6a25435e2b01
Winston Chen <winstonchen.wm@gmail.com>
2021-05-11 14:26:46 -0400

    Navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
-    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5'},
-    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5'},
+    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://images-na.ssl-images-amazon.com/images/I/81iqZ2HHD-L.jpg'},
+    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://images-na.ssl-images-amazon.com/images/I/81iqZ2HHD-L.jpg'},
 ]
 
 const Products = () => {
+    const classes = useStyles();
     return (
-    <main>
+    <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
