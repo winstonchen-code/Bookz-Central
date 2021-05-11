818147a1edcf698be186f5a90d8adc191111702e
Winston Chen <winstonchen.wm@gmail.com>
2021-05-11 01:37:04 -0400

    Card on Screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Products from './components/Products/Products'
 
 const App = () => {
     return (
         <div>
-            Bookz Central
+            <Products />
         </div>
     )
 }
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons'
 
-const Product = () => {
+import useStyles from './styles';
+
+const Product = ({product}) => {
+    const classes = useStyles();
     return (
-        <div>
-            
-        </div>
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography variant="g2" color="textSecondary">{product.description}</Typography>
+                <CardActions disableSpacing className={classes.cardActions}>
+                    <IconButton aria-label="Add to Cart">
+                        <AddShoppingCart/>
+                    </IconButton>
+                </CardActions>
+            </CardContent>
+        </Card>
     )
 }
 
