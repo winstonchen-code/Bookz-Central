fddf861ad78b8d9bd3dd86583e9b7c5644cc35ab
Winston Chen <winstonchen.wm@gmail.com>
2021-05-13 01:02:12 -0400

    Gutter Bottom Cart Update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -38,7 +38,7 @@ const Cart = ({ cart }) => {
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant="h3">Your Shoopping Cart</Typography>
+            <Typography className={classes.title} variant="h3" gutterBottom>Your Shoopping Cart</Typography>
             { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
         </Container>
     )
