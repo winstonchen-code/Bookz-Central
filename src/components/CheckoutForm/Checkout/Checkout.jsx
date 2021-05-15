30e7325146b6ad63610b2adf4e57afad3c352ae8
Winston Chen <winstonchen.wm@gmail.com>
2021-05-15 01:34:22 -0400

    Address Form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -2,12 +2,24 @@ import React, { useState } from 'react'
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
 import useStyles from './styles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
     const classes = useStyles();
+
+    const Confirmation = () => (
+        <div>
+            Confirmation
+        </div>
+    )
+
+    const Form = () => activeStep === 0
+        ? <AddressForm />
+        : <PaymentForm />
 
     return (
         <>
@@ -22,6 +34,7 @@ const Checkout = () => {
                             </Step>
                         ))}
                     </Stepper>
+                    {activeStep === steps.length ? <Confirmation /> : <Form />}
                 </Paper>
             </main>
         </>
